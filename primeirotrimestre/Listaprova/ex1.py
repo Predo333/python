b913a5ac6d154a0ff142ac52730e7e8210a953df
Predo333 <133893193+Predo333@users.noreply.github.com>
2024-03-26 12:22:46 -0300

    Update ex1.py

--- primeirotrimestre/Listaprova/ex1.py
+++ primeirotrimestre/Listaprova/ex1.py
@@ -1,3 +1,6 @@
-x = 32
-y = 55
-print(x+y)
+numero1 = int(input("Digite o primeiro número:"))
+numero2 = int(input("Digite o segundo número:"))
+soma = (numero1+numero2)
+
+
+print("O resultado é:", soma)
