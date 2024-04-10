4c57578347597087f734397f12698531f4a988dc
Predo333 <133893193+Predo333@users.noreply.github.com>
2024-04-10 11:45:46 -0300

    Update ex01_calculadora_imc.py

--- trabalhos/ex01_calculadora_imc.py
+++ trabalhos/ex01_calculadora_imc.py
@@ -1,14 +1,17 @@
 peso=float(input("Digite seu peso: "))
-altura=float(input("Digite sua altura: "))
+altura=float(input("Digite sua altura em metros: "))
 imc=(peso/altura**2)
 
-if imc <=18.5:
-    print("Abaixo do peso")
+if imc <0:
+    print("Insira um valor válido")
     
     #errado#
-elif imc >18.5 and <=24.9: 
-    print("Peso normal")
-
-
-
-
+else:
+    if imc <=18.5: 
+        print("Abaixo do peso")
+    elif imc <=18.5 and imc <24.9:
+        print("Peso Normal")
+    elif imc <=25.0 and imc <29.9:
+        print("Sobrepeso")
+    else:
+        print("kawa gay")
