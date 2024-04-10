bb21b0840d97775ff95de13ab90e2b40d4226b53
Predo333 <133893193+Predo333@users.noreply.github.com>
2024-04-10 12:29:13 -0300

    Update ex01_calculadora_imc.py

--- trabalhos/ex01_calculadora_imc.py
+++ trabalhos/ex01_calculadora_imc.py
@@ -5,7 +5,7 @@ imc=(peso/altura**2)
 if imc <0:
     print("Insira um valor válido")
     
-    #errado#
+
 else:
     if imc <=18.5: 
         print("Abaixo do peso")
@@ -13,5 +13,11 @@ else:
         print("Peso Normal")
     elif imc <=25.0 and imc <29.9:
         print("Sobrepeso")
+    elif imc <=30.0 and imc <34.9:
+        print("Obesidade Grau I")
+    elif imc <=35.0 and imc <39.9:
+        print("Obesidade Grau II (severa)")
     else:
-        print("kawa gay")
+        print("Obesidade Grau III (mórbida)")
+        
+ 
